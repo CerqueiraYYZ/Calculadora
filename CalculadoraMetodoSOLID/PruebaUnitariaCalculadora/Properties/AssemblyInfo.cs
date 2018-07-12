5d395c44d8c2e1b8bcb86c2475f1a290722709d5
CerqueiraYYZ <alfredoacerqueira@gmail.com>
2018-07-12 23:49:53 +0200

    Creacion de base del proyeto
    Se creo las pruebas unitarias y la clase base

---
+++ CalculadoraMetodoSOLID/PruebaUnitariaCalculadora/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaUnitariaCalculadora")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebaUnitariaCalculadora")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d520f18-f5a3-4dc0-a5de-b5bbd0d1d0f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
